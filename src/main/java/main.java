5a1019619fa83c1bcb3212b897b690c771d318f1
jy <jjyung1220@gmail.com>
2022-09-25 17:35:42 +0900

    featureChange/a_b

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -5,7 +5,7 @@ public class main {
         //java
         //dev
         // featureTestPage
-        //feature1_a
+        //feature1_a_b
         SubtractCalculator c1 = new SubtractCalculator();
         c1.setOprands(10,20);
         c1.sum();
